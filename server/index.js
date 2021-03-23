80a73bed130512e71b8eefec993be3dca2254316
Grant Taggart <granttaggartsf@gmail.com>
2021-03-22 19:50:31 -0700

    Complete first phase of proxy

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.get('/:id', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Proxy listening at port ${PORT}`);
+});
